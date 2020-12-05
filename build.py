32b86462cd9840d05be1c5b04cd1784e67a448ab
Alexandr Ivanov <thirvondukr@gmail.com>
2020-12-05 22:54:23 +0300

    Fixed compilation

--- build.py
+++ build.py
@@ -3,14 +3,9 @@ from pathlib import Path
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    'core/__main__.py',
+    'main.py',
     '--onefile',
     '--console',
     '--icon=small_logo.ico',
     '--distpath=./',
 ])
-
-bundle_path = (Path() / '__main__.exe').absolute()
-new_path = bundle_path.parent / 'main.exe'
-new_path.unlink(missing_ok=True)
-bundle_path.rename(new_path)
--- core/main.py
+++ core/main.py
@@ -2,10 +2,7 @@ import datetime
 import sys
 from pathlib import Path
 
-if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-    root_dir = Path().absolute()
-else:
-    root_dir = Path().absolute().parent
 
+root_dir = Path().absolute()
 db_dir = root_dir / 'resources' / 'db'
 start_time = datetime.datetime.now()
