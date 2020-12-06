3510cb291e76ba00a6e16b41f71ea4b5dd391096
Alexandr Ivanov <thirvondukr@gmail.com>
2020-12-07 00:11:03 +0300

    Removed logger.debug

--- mods/tarkov_core/routes/__init__.py
+++ mods/tarkov_core/routes/__init__.py
@@ -2,7 +2,6 @@ from core.logger import logger
 
 
 def init():
-    logger.debug('Initializing tarkov_core  routes')
     import mods.tarkov_core.routes.hideout
     import mods.tarkov_core.routes.lang
     import mods.tarkov_core.routes.profile
