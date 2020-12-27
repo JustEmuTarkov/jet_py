89d8297cbbd3e5d9daa99e502308efe2539b1eef
Doctor <thirvondukr@gmail.com>
2020-12-27 07:52:29 +0300

    Implemented proper bullet splitting into magazine

--- mods/tarkov_core/lib/inventory.py
+++ mods/tarkov_core/lib/inventory.py
@@ -79,6 +79,7 @@ class ImmutableInventory(metaclass=abc.ABCMeta):
 
         :return: Tuple[width, height]
         """
+        # TODO: item folding isn't taken into account
         template = item_templates_repository.get_template(item)
         props = template['_props']
         width = props['Width']
@@ -272,20 +273,49 @@ class InventoryWithGrid(MutableInventory, metaclass=abc.ABCMeta):
             del item['location']
 
         if location['container'] == 'cartridges':
-            mag = self.get_item(location['id'])
-            bullet_stacks_inside_mag = list(self.iter_item_children(mag))
-
-            last_bullet_stack = max(bullet_stacks_inside_mag, key=lambda stack: stack['location'])
-
-            if last_bullet_stack['_tpl'] == item['_tpl']:
-                last_bullet_stack['upd']['StackObjectsCount'] += item['upd']['StackObjectsCount']
-                self.remove_item(item)
-                return
-            else:
-                item['location'] = len(bullet_stacks_inside_mag)
+            magazine = self.get_item(location['id'])
+            self._move_ammo_into_magazine(ammo=item, magazine=magazine)
 
         item['parentId'] = location['id']
         item['slotId'] = location['container']
+
+    def _move_ammo_into_magazine(self, ammo: Item, magazine: Item):
+        bullet_stacks_inside_mag = list(self.iter_item_children(magazine))
+
+        if bullet_stacks_inside_mag:
+            last_bullet_stack = max(bullet_stacks_inside_mag, key=lambda stack: stack['location'])
+
+            # Stack ammo stack with last if possible
+            if last_bullet_stack['_tpl'] == ammo['_tpl']:
+                last_bullet_stack['upd']['StackObjectsCount'] += ammo['upd']['StackObjectsCount']
+                self.remove_item(ammo)
+                return
+
+        # Add new ammo stack to magazine
+        else:
+            ammo['location'] = 0
+        return ammo
+
+    def _split_ammo_into_magazine(self, ammo: Item, magazine: Item):
+        magazine_template = item_templates_repository.get_template(magazine)
+        magazine_capacity: int = magazine_template['_props']['Cartridges'][0]['_max_count']
+
+        bullet_stacks_inside_mag = list(self.iter_item_children(magazine))
+        ammo_to_full = magazine_capacity - sum(stack['upd']['StackObjectsCount'] for stack in bullet_stacks_inside_mag)
+
+        # Remove ammo from inventory if stack fully fits into magazine
+        if ammo['upd']['StackObjectsCount'] <= ammo_to_full:
+            # self.remove_item(ammo)
+            pass
+        # Else if stack is too big to fit into magazine copy ammo and assign it new id and proper stack count
+        else:
+            ammo['upd']['StackObjectsCount'] -= ammo_to_full
+
+            ammo = copy.deepcopy(ammo)
+            ammo['_id'] = generate_item_id()
+            ammo['upd']['StackObjectsCount'] = ammo_to_full
+        self.add_item(ammo)
+        return ammo
 
     def split_item(self, item: Item, location: MoveLocation, count: int) -> Item:
         """
@@ -293,6 +323,12 @@ class InventoryWithGrid(MutableInventory, metaclass=abc.ABCMeta):
 
         :return: New item
         """
+        if location['container'] == 'cartridges':
+            magazine = self.get_item(location['id'])
+            ammo = self._split_ammo_into_magazine(ammo=item, magazine=magazine)
+            # We have to return new ammo stack to the client
+            return ammo
+
         new_item = copy.deepcopy(item)
         new_item['upd']['StackObjectsCount'] = count
         item['upd']['StackObjectsCount'] -= count
