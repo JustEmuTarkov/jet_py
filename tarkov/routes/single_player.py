f03a819b9f20fdecbbf4857b2e3cacd4e43e2f14
Doctor <thirvondukr@gmail.com>
2021-02-24 02:06:28 +0300

    Fixed saving after raid

--- tarkov/routes/single_player.py
+++ tarkov/routes/single_player.py
@@ -3,9 +3,11 @@ from typing import Any, Dict, List
 
 import pydantic
 from fastapi import Request
+from fastapi.params import Depends
 from pydantic import BaseModel, parse_obj_as
 
 from server.utils import make_router
+from tarkov.dependencies import with_profile
 from tarkov.inventory.implementations import SimpleInventory
 from tarkov.inventory.models import Item
 from tarkov.lib import locations
@@ -87,55 +89,57 @@ class ProfileSaveRequest(BaseModel):
 
 
 @singleplayer_router.put("/raid/profile/save")
-async def singleplayer_raid_profile_save(request: Request) -> TarkovSuccessResponse:
+async def singleplayer_raid_profile_save(
+    request: Request,
+    profile: Profile = Depends(with_profile),  # type: ignore
+) -> TarkovSuccessResponse:
     # TODO: Add Saving profile here
     # data struct {exit, isPlayerScav, profile, health}
     # update profile on this request
     body = await request.json()
-    pprint(body, indent=4)
-    with Profile(profile_id=body["profile"]["aid"]) as profile:
-        # profile.pmc_profile['Health']['BodyParts'] = data['health']['Health']
-        # for body_part in profile.pmc_profile['HealtPh']['BodyParts']:
-        #     del body_part['Effects']
-        #
-        # profile.pmc_profile['Health']['Hydration']['Current'] = data['health']['Hydration']
-        # profile.pmc_profile['Health']['Energy']['Current'] = data['health']['Energy']
 
-        # profile.pmc_profile['Info'] = profile_data['Info']
-        # profile.pmc_profile['Encyclopedia'] = profile_data['Encyclopedia']
-        # profile.pmc_profile['Skills'] = profile_data['Skills']
-        raid_profile = body["profile"]
-        profile.pmc_profile.Encyclopedia.update(raid_profile["Encyclopedia"])
-        profile.pmc_profile.Skills = Skills.parse_obj(raid_profile["Skills"])
-        profile.pmc_profile.Quests = pydantic.parse_obj_as(List[Quest], raid_profile["Quests"])
+    # profile.pmc_profile['Health']['BodyParts'] = data['health']['Health']
+    # for body_part in profile.pmc_profile['HealtPh']['BodyParts']:
+    #     del body_part['Effects']
+    #
+    # profile.pmc_profile['Health']['Hydration']['Current'] = data['health']['Hydration']
+    # profile.pmc_profile['Health']['Energy']['Current'] = data['health']['Energy']
 
-        info = raid_profile["Info"]
-        info["LowerNickname"] = profile.pmc_profile.Info.LowerNickname
-        info["GameVersion"] = profile.pmc_profile.Info.GameVersion
-        info["LastTimePlayedAsSavage"] = profile.pmc_profile.Info.LastTimePlayedAsSavage
-        profile.pmc_profile.Info = ProfileInfo.parse_obj(info)
+    # profile.pmc_profile['Info'] = profile_data['Info']
+    # profile.pmc_profile['Encyclopedia'] = profile_data['Encyclopedia']
+    # profile.pmc_profile['Skills'] = profile_data['Skills']
+    raid_profile = body["profile"]
+    profile.pmc_profile.Encyclopedia.update(raid_profile["Encyclopedia"])
+    profile.pmc_profile.Skills = Skills.parse_obj(raid_profile["Skills"])
+    profile.pmc_profile.Quests = pydantic.parse_obj_as(List[Quest], raid_profile["Quests"])
 
-        backend_counters: Dict[str, BackendCounter] = {
-            k: BackendCounter.parse_obj(v)
-            for k, v in raid_profile["BackendCounters"].items()
-            if v["id"] and v["qid"]
-        }
-        for key, raid_counter in backend_counters.items():
-            profile_counter = profile.pmc_profile.BackendCounters.get(key, raid_counter)
-            profile.pmc_profile.BackendCounters[key] = max(raid_counter, profile_counter, key=lambda c: c.value)
+    info = raid_profile["Info"]
+    info["LowerNickname"] = profile.pmc_profile.Info.LowerNickname
+    info["GameVersion"] = profile.pmc_profile.Info.GameVersion
+    info["LastTimePlayedAsSavage"] = profile.pmc_profile.Info.LastTimePlayedAsSavage
+    profile.pmc_profile.Info = ProfileInfo.parse_obj(info)
 
-        profile.pmc_profile.Stats = raid_profile["Stats"]
+    backend_counters: Dict[str, BackendCounter] = {
+        k: BackendCounter.parse_obj(v)
+        for k, v in raid_profile["BackendCounters"].items()
+        if v["id"] and v["qid"]
+    }
+    for key, raid_counter in backend_counters.items():
+        profile_counter = profile.pmc_profile.BackendCounters.get(key, raid_counter)
+        profile.pmc_profile.BackendCounters[key] = max(raid_counter, profile_counter, key=lambda c: c.value)
 
-        raid_inventory_items: List[Item] = parse_obj_as(List[Item], body["profile"]["Inventory"]["items"])
-        raid_inventory = SimpleInventory(items=raid_inventory_items)
-        equipment = profile.inventory.get(profile.inventory.inventory.equipment)
+    profile.pmc_profile.Stats = raid_profile["Stats"]
 
-        # Remove all equipment children
-        profile.inventory.remove_item(equipment, remove_children=True)
+    raid_inventory_items: List[Item] = parse_obj_as(List[Item], body["profile"]["Inventory"]["items"])
+    raid_inventory = SimpleInventory(items=raid_inventory_items)
+    equipment = profile.inventory.get(profile.inventory.inventory.equipment)
 
-        raid_equipment = raid_inventory.iter_item_children_recursively(raid_inventory.get(equipment.id))
-        # regenerate_items_ids(equipment_items)  # Regenerate item ids to be 100% safe
-        profile.inventory.add_item(equipment, list(raid_equipment))
+    # Remove all equipment children
+    profile.inventory.remove_item(equipment, remove_children=True)
+
+    raid_equipment = raid_inventory.iter_item_children_recursively(raid_inventory.get(equipment.id))
+    # regenerate_items_ids(equipment_items)  # Regenerate item ids to be 100% safe
+    profile.inventory.add_item(equipment, list(raid_equipment))
 
     return TarkovSuccessResponse(data=None)
 
