3dd2a4458c5b2431563138ac5efd23dcaf78cb06
Doctor <thirvondukr@gmail.com>
2021-02-24 13:30:33 +0300

    Improved work with paths slightly

--- mods/examplemod/__init__.py
+++ mods/examplemod/__init__.py
@@ -1,21 +1,25 @@
+from typing import ClassVar
+
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.responses import Response
 from starlette.templating import Jinja2Templates
 
-from server.app import app, package_manager
+from server.app import app
 from server.package_lib import PackageBase, PackageMeta
 
 
 sub_api = FastAPI()
-jinja_templates = Jinja2Templates(str(package_manager.packages_dir.joinpath("examplemod", "templates").absolute()))
 
 
-@sub_api.get('/')
-def index(request: Request):
-    return jinja_templates.TemplateResponse("index.html", context={"request": request })
+@sub_api.get("/")
+def index(request: Request) -> Response:
+    return Package.jinja_templates.TemplateResponse("index.html", context={"request": request})
 
 
 class Package(PackageBase):
+    jinja_templates: ClassVar[Jinja2Templates]
+
     class Meta(PackageMeta):
         name: str = "Example Mod"
         version: str = "0.0.1"
@@ -24,4 +28,5 @@ class Package(PackageBase):
         # Any of the tarkov package internals can be manipulated from here
         print("Example mod on_load")
         print("Visit https://127.0.0.1:443/examplemod to see index page")
-        app.mount('/examplemod', sub_api)
+        app.mount("/examplemod", sub_api)
+        Package.jinja_templates = Jinja2Templates(directory=str(self.path.joinpath("templates")))
