91f6bd96b3e5b2f0964eb535deeb48f7bd6701f9
Doctor <thirvondukr@gmail.com>
2021-02-24 11:35:06 +0300

    Added CurrencyEnum

--- tarkov/inventory/types.py
+++ tarkov/inventory/types.py
@@ -1,4 +1,11 @@
+import enum
 from typing import NewType
 
 ItemId = NewType("ItemId", str)
 TemplateId = NewType("TemplateId", str)
+
+
+class CurrencyEnum(enum.Enum):
+    USD = "5696686a4bdc2da3298b456a"
+    RUB = "5449016a4bdc2d6f028b456f"
+    EUR = "569668774bdc2da2298b4568"
