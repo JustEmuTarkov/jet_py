feadab01b55250c886c107a3f6cffb0ffbcca8b6
Doctor <thirvondukr@gmail.com>
2021-02-24 13:21:10 +0300

    Created mod example

---
+++ mods/examplemod/__init__.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+from server.app import app, package_manager
+from server.package_lib import PackageBase, PackageMeta
+
+
+sub_api = FastAPI()
+jinja_templates = Jinja2Templates(str(package_manager.packages_dir.joinpath("examplemod", "templates").absolute()))
+
+
+@sub_api.get('/')
+def index(request: Request):
+    return jinja_templates.TemplateResponse("index.html", context={"request": request })
+
+
+class Package(PackageBase):
+    class Meta(PackageMeta):
+        name: str = "Example Mod"
+        version: str = "0.0.1"
+
+    def on_load(self) -> None:
+        print("Example mod on_load")
+        print("Visit https://127.0.0.1:443/examplemod to see index page")
+        app.mount('/examplemod', sub_api)
