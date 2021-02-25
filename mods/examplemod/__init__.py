6a557a9f34f73869b7bc095cf20a19d4a5e8c8f1
Doctor <thirvondukr@gmail.com>
2021-02-25 11:14:54 +0300

    Removed unused imports

--- mods/examplemod/__init__.py
+++ mods/examplemod/__init__.py
@@ -1,7 +1,3 @@
-from typing import ClassVar
-
-from starlette.templating import Jinja2Templates
-
 from server.package_lib import PackageBase, PackageMeta
 from tarkov.hideout import repositories as hideout_repositories
 
@@ -12,8 +8,6 @@ def set_production_time_to_1():
 
 
 class Package(PackageBase):
-    jinja_templates: ClassVar[Jinja2Templates]
-
     class Meta(PackageMeta):
         name: str = "Example Mod"
         version: str = "0.0.1"
