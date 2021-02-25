54035cb12295a83b56d0a16705ca1d8bd79f61a7
Doctor <thirvondukr@gmail.com>
2021-02-25 15:18:51 +0300

    Fixed hideout repositories __init__

--- tarkov/hideout/repositories/__init__.py
+++ tarkov/hideout/repositories/__init__.py
@@ -1,9 +1,4 @@
-from tarkov.hideout.repositories.areas import areas_repository
-from tarkov.hideout.repositories.production import production_repository
-from tarkov.hideout.repositories.scavcase_production import scavcase_production_repository
-import tarkov.hideout.repositories.settings
-
-areas = areas_repository
-production = production_repository
-scavcase_production = scavcase_production_repository
-settings = tarkov.hideout.repositories.settings.settings
+from .areas import areas_repository
+from .production import production_repository
+from .scavcase_production import scavcase_production_repository
+from .settings import settings
