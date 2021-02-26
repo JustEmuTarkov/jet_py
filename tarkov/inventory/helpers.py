c31fa43cd2db19c636cef6ad71683e41d8f97a27
Doctor <thirvondukr@gmail.com>
2021-02-26 10:54:51 +0300

    Id regeneration fix

--- tarkov/inventory/helpers.py
+++ tarkov/inventory/helpers.py
@@ -31,8 +31,6 @@ def regenerate_items_ids(items: List["Item"]) -> None:
 
 
 def regenerate_item_ids_dict(items: List[Dict]) -> None:
-    items = [item for item in items if "parentId" in item]
-
     id_map: Dict["ItemId", "ItemId"] = {item["_id"]: generate_item_id() for item in items}
 
     for item in items:
