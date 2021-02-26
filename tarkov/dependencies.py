2fb717082f3645c1499c1dc4290dbfeb3326bb8d
Doctor <thirvondukr@gmail.com>
2021-02-26 22:26:43 +0300

    Moved profile saving into background task

--- tarkov/dependencies.py
+++ tarkov/dependencies.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from typing import AsyncIterable, Dict
 
 from fastapi.params import Cookie
+from fastapi import BackgroundTasks
 
 from server import logger
 from tarkov.profile import Profile
@@ -12,20 +13,26 @@ locks: Dict[str, Lock] = defaultdict(Lock)
 profiles: Dict[str, Profile] = {}
 
 
+async def save_profile_task(profile: Profile) -> None:
+    assert locks[profile.profile_id].locked()
+    profile.write()
+
+
 async def with_profile(
+    background_tasks: BackgroundTasks,
     profile_id: str = Cookie(..., alias="PHPSESSID"),  # type: ignore
 ) -> AsyncIterable[Profile]:
-    async with locks[profile_id]:
-        if profile_id not in profiles:
-            # Create profile instance if it doesn't exists
-            profiles[profile_id] = Profile(profile_id)
-            profiles[profile_id].read()
+    if profile_id not in profiles:
+        profiles[profile_id] = Profile(profile_id)
+        profiles[profile_id].read()
+    profile = profiles[profile_id]
 
+    # After completing request flush profile to drive
+    background_tasks.add_task(save_profile_task, profile)
+    async with locks[profile_id]:
         try:
-            profiles[profile_id].update()
+            profiles.update()
             yield profiles[profile_id]
-            # After completing request write profile if everything is ok
-            profiles[profile_id].write()
 
         except Exception as error:
             # Else read it again from filesystem
