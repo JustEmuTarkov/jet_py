2f3a96d7174dc13debf02c9a987e2625a631457e
Doctor <thirvondukr@gmail.com>
2021-06-12 23:14:44 +0300

    Integrate insurance into /raid/profile/save route

--- tarkov/offraid/router.py
+++ tarkov/offraid/router.py
@@ -2,9 +2,9 @@ from dependency_injector.wiring import Provide, inject
 from fastapi.params import Depends
 
 from server.container import AppContainer
-from server.requests import ZLibRequest
 from server.utils import make_router
 from tarkov.dependencies import profile_manager
+from tarkov.insurance.interfaces import IInsuranceService
 from tarkov.models import TarkovSuccessResponse
 from tarkov.offraid.requests import OffraidSaveRequest
 from tarkov.offraid.services import OffraidSaveService
@@ -19,9 +19,22 @@ def singleplayer_raid_profile_save(
     request: OffraidSaveRequest,
     profile: Profile = Depends(profile_manager.with_profile),
     offraid_service: OffraidSaveService = Depends(Provide[AppContainer.offraid.service]),
+    insurance_service: IInsuranceService = Depends(Provide[AppContainer.insurance.service]),
 ) -> TarkovSuccessResponse:
     if request.is_player_scav:
         raise NotImplementedError
+
+    if not request.health.is_alive:
+        insured_items = insurance_service.get_lost_insured_items(
+            profile=profile,
+            offraid_profile=request.profile,
+        )
+        for trader_id, items in insured_items.items():
+            insurance_service.send_insurance_mail(
+                items=items,
+                trader_id=trader_id,
+                profile=profile,
+            )
 
     offraid_service.update_profile(
         profile=profile,
