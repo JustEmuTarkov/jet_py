af935fc3da4e0af750a53e3f190ae1d66f9be2b4
Doctor <thirvondukr@gmail.com>
2021-06-12 23:13:06 +0300

    Create Interface for InsuranceService

---
+++ tarkov/insurance/interfaces.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import abc
+from typing import Dict, List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tarkov.inventory.models import Item
+    from tarkov.offraid.models import OffraidProfile
+    from tarkov.profile.profile import Profile
+    from tarkov.trader.models import ItemInsurance
+    from tarkov.trader.types import TraderId
+
+
+class IInsuranceService(abc.ABC):
+    @abc.abstractmethod
+    def is_item_insured(self, item: Item, profile: Profile) -> bool: ...
+
+    @abc.abstractmethod
+    def insurance_info(self, item: Item, profile: Profile) -> ItemInsurance: ...
+
+    @abc.abstractmethod
+    def get_lost_insured_items(
+        self,
+        profile: Profile,
+        offraid_profile: OffraidProfile,
+    ) -> Dict[TraderId, List[Item]]: ...
+
+    @abc.abstractmethod
+    def send_insurance_mail(
+        self,
+        items: List[Item],
+        trader_id: TraderId,
+        profile: Profile
+    ) -> None: ...
